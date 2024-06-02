d26030104f4d5ed9ad2c83605b329d7ecda8f627
MonzerOmer1234 <monzeromer048@gmail.com>
2024-06-02 21:06:21 +0200

    fix errors

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import GlobalState from './store/GlobalState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/Food_Recipe_App'>
+  <HashRouter>
   <React.StrictMode>
     <GlobalState><App /></GlobalState>
   </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
