3b4e5dec7aca1ab4b83fc889c2c8f5e98ff9bd63
MonzerOmer1234 <monzeromer048@gmail.com>
2024-06-02 11:34:12 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import GlobalState from './store/GlobalState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter >
   <React.StrictMode>
     <GlobalState><App /></GlobalState>
   </React.StrictMode>
